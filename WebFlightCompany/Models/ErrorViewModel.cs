df67bdda9104a840fde0e4ebf6b250333cb0f9e5
githubDlyaHuesosovI314Idarasov <thepokemontrainergold@gmail.com>
2024-10-15 16:51:13 +0300

    Revert "Reapply "Добавьте файлы проекта.""
    This reverts commit ef1a9ff2bbabcc97bcb87b9d097a9472705acc7e.

--- WebFlightCompany/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace WebFlightCompany.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
